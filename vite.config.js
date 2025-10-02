194d5609a8715e04b41c8fd6351066b8c3e2a94e
Andrea Corti <ascorti@softeam.it>
2025-10-02 16:58:30 +0200

    test pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',
+  build: {
+    outDir: 'docs'
+  },
   server: {
     port: 3000,
     open: true
